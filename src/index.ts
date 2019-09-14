97de4d5d895a9ec2f89ed2832778e7d25b9694d7
Daniel Dietrich <cafebab3@gmail.com>
2019-09-14 14:48:25 +0200

    Precompiling all regular expressions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+const leadingWhitespace = /^(s*\n)+/;
+const newline = /\n/g;
+const nonWhitespace = /\S|$/;
 /**
  * A tag function that automatically aligns embedded multiline strings.
  *
@@ -7,8 +10,7 @@
  */
 export = function s(staticParts: TemplateStringsArray, ...substitutions: unknown[]): string {
     const lines = substitutions                      // align substitutions and fuse them with static parts
-        .map(val => String(val))
-        .reduce((acc, subst, i) => acc + align(subst, acc) + staticParts[i + 1], staticParts[0])
+        .reduce((acc: string, subst, i) => acc + align(String(subst), acc) + staticParts[i + 1], staticParts[0])
         .split('\n')                                 // converts text to lines
         .map(line => line.trimRight());              // whitespace-only lines are empty (preserving leading whitespace)
     const indent = findIndentation(lines);           // finds the minimum indentation
@@ -16,19 +18,19 @@ export = function s(staticParts: TemplateStringsArray, ...substitutions: unknown
         .map(line => line.slice(indent).trimRight()) // shifts lines to the left
         .join('\n')                                  // convert lines to string
         .trimRight()                                 // removes trailing whitespace after joining possibly empty lines
-        .replace(/^(s*\n)+/, '');                    // removes leading whitespace while preserving indentation
+        .replace(leadingWhitespace, '');                    // removes leading whitespace while preserving indentation
 };
 
 // add the alignment of the previous static part to all lines of the following substitution
 function align(subst: string, acc: string): string {
     const length = Math.max(0, acc.length - acc.lastIndexOf('\n') - 1);
     const indent = ' '.repeat(length);
-    return subst.replace(/\n/g, '\n' + indent);
+    return subst.replace(newline, '\n' + indent);
 }
 
 // finds the indentation of a text block represented by a sequence of lines
 function findIndentation(lines: string[]): number {
-    const indents = lines.filter(line => line.length > 0).map(line => line.search(/\S|$/));
+    const indents = lines.filter(line => line.length > 0).map(line => line.search(nonWhitespace));
     const min = indents.length === 0 ? 0 : Math.min(...indents); // min(...[]) = min() = Infinity
     return Math.max(0, min);
 }
